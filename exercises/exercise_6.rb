751896fbe61b36d0a7987677372e66432cad714b
Drew Kang <kangdrew95@gmail.com>
2021-09-14 02:56:47 +0000

    Completed exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Erlich", last_name: "Bachman", hourly_rate: 69)
+@store1.employees.create(first_name: "Jian", last_name: "Yang", hourly_rate: 40)
+@store2.employees.create(first_name: "Richard", last_name: "Hendricks", hourly_rate: 80)
+@store2.employees.create(first_name: "Dinesh", last_name: "Chugtai", hourly_rate: 100)
+@store2.employees.create(first_name: "Bertram", last_name: "Gilfoyle", hourly_rate: 70)
+@store2.employees.create(first_name: "Jared", last_name: "Dunn", hourly_rate: 90)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,17 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true }
+  validates :annual_revenue, length: { in: 0..INFINITY..1000000 }
+
+  validates :men_women_false
+
+  def men_women_false
+    if !mens_apparel && !womens_apparel
+      errors.add("The store must sell either or both men/womens apparel.")
+    end
+
+  end
+
 
 end
