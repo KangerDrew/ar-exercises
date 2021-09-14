75c087bcee3aef481eb2844de043727741fd35de
Drew Kang <kangdrew95@gmail.com>
2021-09-14 02:21:44 +0000

    Fixed exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -13,15 +13,14 @@ whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel:
 yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
 @mens_stores = Store.where(mens_apparel: true)
+@womens_stores_under_1m = Store.where(womens_apparel: true, annual_revenue: -Float::INFINITY..1000000 )
 
-"----------------STORES SELLING MENS APPAREL----------------"
+puts "----------------STORES SELLING MENS APPAREL----------------"
 @mens_stores.each do |store|
   puts "The store name is #{store.name}, and their annual revenue is #{store.annual_revenue}."
 end
 
-@womens_stores_under_1m = Store.where(womens_apparel: true, annual_revenue: -Float::INFINITY..1000000 )
-
-"----------STORES SELLING WOMENS APPAREL UNDER $1M----------"
+puts "----------STORES SELLING WOMENS APPAREL UNDER $1M----------"
 @womens_stores_under_1m.each do |store|
   puts "The store name is #{store.name}, and their annual revenue is #{store.annual_revenue}."
 end
