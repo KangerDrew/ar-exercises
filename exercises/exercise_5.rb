56ec57b227ea78407a0757bf08c04c87b98c04e1
Drew Kang <kangdrew95@gmail.com>
2021-09-14 02:32:56 +0000

    Completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "----------------TOTAL REVENUE----------------"
+puts "The total revenue of the company is: $#{Store.sum(:annual_revenue)}."
+puts "---------------AVERAGE REVENUE---------------"
+puts "Average revenue is: $#{Store.sum(:annual_revenue)/Store.count}."
+puts "------# OF STORES WITH <$1M ANNUAL SALES------"
+puts "#{Store.where(annual_revenue: 1000000..Float::INFINITY).count} stores!"
